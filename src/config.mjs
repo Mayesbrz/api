817309bce33f3ed2d1708291e4c7f49b8a86a566
Mayesbrz <amayas.bariz@gmail.com>
2025-05-19 11:28:51 +0200

    update : api commit 1

---
+++ src/config.mjs
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://amayas:amayas@efrei.fl8c5.mongodb.net/'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://amayas:amayas@efrei.fl8c5.mongodb.net/'
+  }
+};
